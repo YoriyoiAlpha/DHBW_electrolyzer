235b0b071f6a988c50cd7b760c25ad62f8e383ef
leo <leo.lemaire007@orange.fr>
2024-06-17 15:59:57 +0200

    InfluxDB -> Prometheus PushGateway

--- main.py
+++ main.py
@@ -1,33 +1,31 @@
 import pyModbusTCP
 from Electrolyzer import Electrolyzer
 
-import influxdb_client, os, time
-from influxdb_client import InfluxDBClient, Point, WritePrecision
-from influxdb_client.client.write_api import SYNCHRONOUS
+import time
+from prometheus_client import CollectorRegistry, Gauge, push_to_gateway
+import re
 
-d = Electrolyzer("localhost", 4502, 1)
+d = Electrolyzer("192.168.0.2", 502, 1)
 d.get_input_defs_from_JSON("input_registers.json")
 d.connect()
 print(d.dump_all_in_reg())
 
-token = os.environ.get("INFLUXDB_TOKEN")
-org = "DHBW"
-url = "https://dhbw-influx.leserveurdansmongrenier.uk"
-
-write_client = influxdb_client.InfluxDBClient(url=url, token=token, org=org)
-
-bucket="electrolyzer"
-
-write_api = write_client.write_api(write_options=SYNCHRONOUS)
+url = "http://141.72.13.23:9091"
 
 while True:
+    t = time.time()
     data = d.dump_all_in_reg()
+
+    registry = CollectorRegistry()
     for v in data:
         if(data[v] == NotImplemented):
             data[v] = -1
+        # data[v] = str(data[v])
         # if int larger than 64 bits convert it to string because influxDB only support int64
-        if(type(data[v]) == int and data[v].bit_length() > 64):
-            data[v] = str(data[v])
-        point = Point("electrolyser").field(v, data[v])
-        write_api.write(bucket=bucket, org="DHBW", record=point)
-    time.sleep(1)
+        # if(type(data[v]) == int and data[v].bit_length() > 64):
+            # data[v] = str(data[v])
+        g = Gauge(re.sub(r'[\W_]+','_',v), v, registry=registry)
+        g.set(data[v])
+
+    push_to_gateway(url, job='electrolyzer', registry=registry)
+    print(time.time() - t)
